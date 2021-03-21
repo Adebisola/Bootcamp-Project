25f207834d7b24f0ad743e484e9d8cc624fc6977
Adebisola <adeniyiadebisola@gmail.comssh-keygen -t rsa -C adeniyiadebisola@gmail.comls -al ~/.sshssh-keygen -t rsa -b 4096 -C adeniyiadebisola@gmail.com>
2021-03-21 15:57:25 +0100

    added JS

---
+++ index.js
@@ -0,0 +1,5 @@
+const togglenavbar = document.getElementById('togglenavbar');
+const navigation = document.getElementById('navigation');
+togglenavbar.addEventListener('click', () => {
+navigation.classList.toggle('show');
+});
